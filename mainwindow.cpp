9803069668376a5c4bf61398a64c337d9bed1cdb
Leonardo Iarossi <leonardo.iarossi@icloud.com>
2025-08-18 12:27:59 +0200

    quando i g > 1, lo sfondo diventa rosso per 2 secondi

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,16 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <iostream>
+#include <QTimer>
+
+ QTimer timer; //un timer
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
@@ -15,6 +19,12 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::scriviDati(float dati[]){
-    std::cout<<"dati"<<std::endl;
+    QObject::connect(&timer, &QTimer::timeout, [&]() { //ogni volta che il timer finisce (evento timeout), eseguo la funzione passando tutte le variabili di contesto con [&]
+        ui->lAX->setStyleSheet({"QLabel {background-color: white;}"});
+    });
+    if((dati[0] > 1) || (dati[1] > 1) || (dati[2] > 1)){
+        ui->lAX->setStyleSheet({"QLabel {background-color: red;}"});
+        timer.start(2000);
+    }
     ui->lAX->setText("acc" + QString::number(dati[0], 'f', 2) + "g\t" + QString::number(dati[1], 'f', 2) + "g\t" + QString::number(dati[2], 'f', 2) + "g\t");
 }
