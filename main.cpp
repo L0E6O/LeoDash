1faef6b3db6665088c9a896c225ad945777659af
Leonardo Iarossi <leonardo.iarossi@icloud.com>
2025-08-18 12:11:30 +0200

    scambiato loop infinito con QTimer

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "datagatherer.h"
 #include <unistd.h>
 #include <QApplication>
+#include <QTimer>
 
 
 int main(int argc, char *argv[])
@@ -13,13 +14,18 @@ int main(int argc, char *argv[])
     int16_t datiRaw[6];
     float dati[6];
     w.show();
-    while(!terminator){
+
+    QTimer timer; //un timer
+    QObject::connect(&timer, &QTimer::timeout, [&]() { //ogni volta che il timer finisce (evento timeout), eseguo la funzione passando tutte le variabili di contesto con [&]
+        int16_t datiRaw[6];
+        float dati[6];
         dG.prendiDati(datiRaw);
-        for(int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             dati[i] = datiRaw[i] / 16384.0f;
         }
         w.scriviDati(dati);
-        usleep(100000);
-    }
+    });
+
+    timer.start(100); // ogni 100 ms (come usleep(100000))
     return a.exec();
 }
