c68cf6bc27af9a6a961643faf45df33620a90524
Leonardo Iarossi <leonardo.iarossi@icloud.com>
2025-08-22 17:33:04 +0200

    cambiato sfondo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,10 +12,10 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     ui->centralwidget->setStyleSheet("#centralwidget { background-image: url(./clio.png); }");
     ui->Tabs->setStyleSheet("QWidget{background:transparent} QTabWidget::pane{border: 1px;border-color:red;background-color: transparent;} QTabBar::tab {background-color: transparent;color: #ccc;width: 280px;height:48px;font-size:24px} QTabBar::tab:hover{background-color:#ddd; color: white;} QTabBar::tab:selected{background-color: #363535; color: #008BEA;}");
-    ui->lAX->setStyleSheet({"#lAX {background-color: rgb(0,0,0); color: white; border-radius: 30px;}"});
-    ui->lAY->setStyleSheet({"#lAY {background-color: rgb(0,0,0); color: white; border-radius: 30px;}"});
-    ui->lGX->setStyleSheet({"#lGX {background-color: rgb(0,0,0); color: white; border-radius: 30px;}"});
-    ui->lGY->setStyleSheet({"#lGY {background-color: rgb(0,0,0); color: white; border-radius: 30px;}"});
+    ui->lAX->setStyleSheet({"#lAX {background-color: rgba(0,0,0,0); color: white;}"});
+    ui->lAY->setStyleSheet({"#lAY {background-color: rgba(0,0,0,0); color: white;}"});
+    ui->lGX->setStyleSheet({"#lGX {background-color: rgba(0,0,0,0); color: white;}"});
+    ui->lGY->setStyleSheet({"#lGY {background-color: rgba(0,0,0,0); color: white;}"});
 }
 
 MainWindow::~MainWindow()
@@ -25,18 +25,18 @@ MainWindow::~MainWindow()
 
 void MainWindow::scriviDati(float dati[]){
     QObject::connect(&timerX, &QTimer::timeout, [&]() { //ogni volta che il timer finisce (evento timeout), eseguo la funzione passando tutte le variabili di contesto con [&]
-        ui->lAX->setStyleSheet({"#lAX {background-color: rgb(0,0,0); color: white; border-radius: 30px;}"});
+        ui->lAX->setStyleSheet({"#lAX {color: white;}"});
     });
     QObject::connect(&timerY, &QTimer::timeout, [&]() { //ogni volta che il timer finisce (evento timeout), eseguo la funzione passando tutte le variabili di contesto con [&]
-        ui->lAY->setStyleSheet({"#lAY {background-color: rgb(0,0,0); color: white; border-radius: 30px;}"});
+        ui->lAY->setStyleSheet({"#lAY {color: white;}"});
     });
 
     if((dati[0] > 7) || (dati[0] < -7)){
-        ui->lAX->setStyleSheet({"#lAX {background-color: red; border-radius: 30px;}"});
+        ui->lAX->setStyleSheet({"#lAX {color: red;}"});
         timerX.start(2000);
     }
     if((dati[1] > 7) || (dati[1] < -7)){
-        ui->lAY->setStyleSheet({"#lAY {background-color: red; border-radius: 30px;}"});
+        ui->lAY->setStyleSheet({"#lAY {color: red;}"});
         timerY.start(2000);
     }
     ui->lAX->setText("accX" + QString::number(dati[0], 'f', 2));
