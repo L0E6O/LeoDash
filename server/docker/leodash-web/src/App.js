156d3bcccedaaf3e0795b81f9ab64dfe6890495d
L0E6O <leonardo.iarossi@icloud.com>
2025-08-28 17:53:16 +0200

    creata struttura di base per webapp

--- server/docker/leodash-web/src/App.js
+++ server/docker/leodash-web/src/App.js
@@ -1,26 +1,21 @@
 import './App.css';
+import Quadrante from './Quadrante'
 import { useEffect, useState } from 'react';
 
 function App() {
   const [data, setData] = useState(null);
 
-  const hostIp = process.env.REACT_APP_API_URL;
+  const hostIp = process.env.REACT_APP_API_IP;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        
-        console.log(hostIp);
 
-        const response = await fetch(`http://${hostIp}:8080/api/MPU6050/data/`, {method: "GET"});
+        const response = await fetch(`http://${hostIp}:8080/api/MPU6050/data/`, { method: "GET" });
 
-        console.log(response);
-        
-        
         if (response.ok) {
           const result = await response.json();
           setData(result);
-          console.log(result);
         } else {
           console.error('Failed to fetch data:', response.status);
         }
@@ -42,7 +37,11 @@ function App() {
   return (
     <div className="App">
       {data ? (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        <div className="sensor-div">
+          {Object.keys(data).map((key, index) => {
+            return <Quadrante name={key} data={data[key]} />;
+          })}
+        </div>
       ) : (
         <p>Loading data...</p>
       )}
---
+++ server/docker/leodash-web/src/Quadrante.js
@@ -0,0 +1,11 @@
+function Quadrante({ name, data }){
+
+    return (
+        <div>
+            <h3>{name}</h3>
+            <p>{data}</p>
+        </div>
+    )
+}
+
+export default Quadrante;
